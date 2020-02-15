588def62ad00d5f441c17aad88a54bcff5a30d42
Supanat Pothivarakorn <supanat.ptk@gmail.com>
2020-02-15 17:44:34 +0700

    Refactor LinerFilter logic out

---
+++ Algorithm/LinearFilter.cs
@@ -0,0 +1,61 @@
+using System;
+using MathNet.Filtering.Kalman;
+using MathNet.Numerics.LinearAlgebra;
+
+namespace KfPairs.Algorithm
+{
+    public class LinearFilter
+    {
+        private readonly IKalmanFilter _kalmanFilter;
+
+        public double Slope => _kalmanFilter.State[0, 0];
+        public double Intercept => _kalmanFilter.State[1, 0];
+
+        public LinearFilter()
+        {
+            var initStateEstimate = Matrix(new[,]
+            {
+                {0.0},
+                {0.0}
+            });
+
+            var initStateCov = Matrix(new[,]
+            {
+                {1.0, 0.0},
+                {0.0, 1.0}
+            });
+
+            _kalmanFilter = new SquareRootFilter(initStateEstimate, initStateCov);
+        }
+        
+        public void Update(double y, double x)
+        {
+            var measurement = Matrix(new[,]
+            {
+                {y}
+            });
+
+            var measurementModel = Matrix(new[,]
+            {
+                {x, 1.0}
+            });
+
+            var cov = Matrix(new[,]
+            {
+                {1.0}
+            });
+
+            _kalmanFilter.Update(measurement, measurementModel, cov);
+        }
+
+        public double Predict(double x)
+        {
+            return (Slope * x) + Intercept;
+        }
+
+        private Matrix<double> Matrix(double[,] arr)
+        {
+            return Matrix<double>.Build.DenseOfArray(arr);
+        }
+    }
+}
